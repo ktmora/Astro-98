fe4b77869e9271386b5c5f3482bdbb0b17817dc8
ktmora <katherinemora@berkeley.edu>
2024-01-29 15:56:23 -0800

    added hw1.py

---
+++ hw1.py
@@ -0,0 +1,5 @@
+print(63837+46667+4483*968*87)
+#to run, go into terminal, make sure you're in the correct folder (for me that is python_files), and enter: python3 hw1.py
+#the equation is a silly little phrase, if you can decipher it :)
+#I think the phrase is self explanatory
+#also the name of the files is hw1.py, because.. its the first homework. 
