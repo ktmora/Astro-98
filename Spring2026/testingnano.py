d39a7e389b9e9dac3eaffe5cb94e1d79258dd75d
ktmora <katherinemora@berkeley.edu>
2026-02-08 14:00:08 -0800

    adding testingnano.py and class3demo.py

---
+++ Spring2026/testingnano.py
@@ -0,0 +1,13 @@
+# File : testingnano.py
+
+print(5+5) # prints 5 plus 5
+
+print( 5 + 5 ) # spacing makes code easier to read! 
+
+print(5 +5) # keep spacing consistent 
+
+print(5 + 5) 
+
+print(5 + 5 ** 2) 
+
+# testing saving our work
